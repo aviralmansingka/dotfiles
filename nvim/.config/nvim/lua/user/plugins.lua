89671c08fc3a36461f8a23dd8ff27795c32721e5
amansingka <amansingka@apple.com>
2022-07-31 16:35:11 +0530

    Simplifying package management and cleaning up

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -1,95 +1,114 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
 vim.cmd([[
-" BASIC VIMRC SKELETON
-" Owner: Aviral Mansingka (github:aviralmansingka)
-
-" Start adding plugins
-call plug#begin('~/.config/nvim/plugged')
-
-Plug 'declancm/maximize.nvim'
-
-" General plugin used by everyone
-Plug 'nvim-lua/popup.nvim'
-Plug 'nvim-lua/plenary.nvim'
-
-" appearences and themes
-Plug 'ellisonleao/gruvbox.nvim'
-
-" icons
-Plug 'kyazdani42/nvim-web-devicons'
-Plug 'yamatsum/nvim-nonicons'
-
-" statusline tabline
-Plug 'akinsho/bufferline.nvim', { 'tag': 'v2.*' }
-Plug 'nvim-lualine/lualine.nvim'
-
-" after/plugin/nvim-tree.rc.vim
-Plug 'kyazdani42/nvim-tree.lua'
-
-" after/plugin/telescope.rc.vim
-Plug 'nvim-telescope/telescope.nvim'
-Plug 'jvgrootveld/telescope-zoxide'
-
-" after/plugin/vim-rooter.rc.vim
-Plug 'airblade/vim-rooter'
-
-" after/plugin/startify.rc.vim
-Plug 'mhinz/vim-startify'
-
-" after/plugin/treesitter.rc.vim
-Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
-Plug 'nvim-treesitter/nvim-treesitter-refactor'
-Plug 'nvim-treesitter/nvim-treesitter-textobjects'
-Plug 'nvim-treesitter/playground'
-Plug 'andymass/vim-matchup'
-Plug 'windwp/nvim-ts-autotag'
-Plug 'windwp/nvim-autopairs'
-
-" after/plugin/git.rc.vim
-Plug 'tpope/vim-fugitive'
-Plug 'kdheepak/lazygit.nvim'
-Plug 'lewis6991/gitsigns.nvim'
-Plug 'tpope/vim-rhubarb'
-
-" comments
-Plug 'numToStr/Comment.nvim'
-
-" after/plugin/snippets.rc.vim
-Plug 'L3MON4D3/LuaSnip'
-Plug 'rafamadriz/friendly-snippets'
-
-" after/plugin/nvim-cmp.rc.vim
-Plug 'hrsh7th/nvim-cmp'
-Plug 'hrsh7th/cmp-buffer'
-Plug 'hrsh7th/cmp-path'
-Plug 'hrsh7th/cmp-nvim-lua'
-Plug 'hrsh7th/cmp-nvim-lsp'
-Plug 'saadparwaiz1/cmp_luasnip'
-Plug 'onsails/lspkind.nvim'
-
-" after/plugin/nvim-lspconfig.rc.vim
-Plug 'ray-x/lsp_signature.nvim'
-Plug 'neovim/nvim-lspconfig'
-Plug 'jose-elias-alvarez/null-ls.nvim'
-Plug 'jose-elias-alvarez/nvim-lsp-ts-utils'
-Plug 'gbrlsnchs/telescope-lsp-handlers.nvim'
-
-" after/plugin/vim-floaterm
-Plug 'voldikss/vim-floaterm'
-Plug 'akinsho/toggleterm.nvim'
-
-" after/plugin/test.tc.vim
-Plug 'vim-test/vim-test'
-Plug 'rcarriga/vim-ultest', { 'do': ':UpdateRemotePlugins' }
-
-" after/plugin/nvim-dap.rc.vim
-Plug 'mfussenegger/nvim-dap'
-Plug 'leoluz/nvim-dap-go'
-Plug 'theHamsta/nvim-dap-virtual-text'
-Plug 'rcarriga/nvim-dap-ui'
-" TODO: Plug 'nvim-telescope/telescope-dap.nvim'
-
-Plug 'aserowy/tmux.nvim'
-Plug 'fladson/vim-kitty'
-
-call plug#end()
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
 ]])
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+	return
+end
+
+-- Have packer use a popup window
+packer.init({
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+})
+return packer.startup(function(use)
+	use({ "wbthomason/packer.nvim" })
+	use("declancm/maximize.nvim")
+
+	use("nvim-lua/popup.nvim")
+	use("nvim-lua/plenary.nvim")
+
+	use("ellisonleao/gruvbox.nvim")
+
+	use("kyazdani42/nvim-web-devicons")
+	use("yamatsum/nvim-nonicons")
+
+	use("akinsho/bufferline.nvim")
+	use("nvim-lualine/lualine.nvim")
+
+	use("kyazdani42/nvim-tree.lua")
+
+	use("nvim-telescope/telescope.nvim")
+	use("jvgrootveld/telescope-zoxide")
+
+	use("airblade/vim-rooter")
+
+	use("mhinz/vim-startify")
+
+	use("nvim-treesitter/nvim-treesitter")
+	use("nvim-treesitter/nvim-treesitter-refactor")
+	use("nvim-treesitter/nvim-treesitter-textobjects")
+	use("nvim-treesitter/playground")
+	use("andymass/vim-matchup")
+	use("windwp/nvim-ts-autotag")
+	use("windwp/nvim-autopairs")
+
+	use("tpope/vim-fugitive")
+	use("kdheepak/lazygit.nvim")
+	use("lewis6991/gitsigns.nvim")
+	use("tpope/vim-rhubarb")
+
+	use("numToStr/Comment.nvim")
+
+	use("L3MON4D3/LuaSnip")
+	use("rafamadriz/friendly-snippets")
+
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("hrsh7th/cmp-nvim-lua")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("saadparwaiz1/cmp_luasnip")
+	use("onsails/lspkind.nvim")
+
+	use("ray-x/lsp_signature.nvim")
+	use("neovim/nvim-lspconfig")
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("jose-elias-alvarez/nvim-lsp-ts-utils")
+	use("gbrlsnchs/telescope-lsp-handlers.nvim")
+
+	use("voldikss/vim-floaterm")
+	use("akinsho/toggleterm.nvim")
+
+	use({ "rcarriga/vim-ultest", requires = { "vim-test/vim-test" }, run = ":UpdateRemotePlugins" })
+
+	use("mfussenegger/nvim-dap")
+	use("leoluz/nvim-dap-go")
+	use("theHamsta/nvim-dap-virtual-text")
+	use("rcarriga/nvim-dap-ui")
+
+	use("aserowy/tmux.nvim")
+	use("fladson/vim-kitty")
+
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
+end)
