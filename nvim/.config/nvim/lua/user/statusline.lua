75592c4cff1056ddd24c9fe23d84ff795ee843fe
amansingka <amansingka@apple.com>
2022-08-22 20:27:45 -0700

    Updating bufferline prefs

--- nvim/.config/nvim/lua/user/statusline.lua
+++ nvim/.config/nvim/lua/user/statusline.lua
@@ -3,7 +3,7 @@ require("bufferline").setup({
 	options = {
 		diagnostics = "nvim_lsp",
 		offsets = { { filetype = "NvimTree", text = "File Explorer", text_align = "left" } },
-		separator_style = "thick",
+		separator_style = "slant",
 		diagnostics_indicator = function(count, level, diagnostics_dict, context)
 			local icon = level:match("error") and " " or " "
 			return " " .. icon .. count
