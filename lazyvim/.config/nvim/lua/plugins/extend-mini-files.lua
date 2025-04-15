24e55e7aab6668cc6702265783801d2d74fb710b
aviralmansingka <aviralmansingka@gmail.com>
2025-04-15 18:14:51 +0000

    Updating mini.files usage

---
+++ lazyvim/.config/nvim/lua/plugins/extend-mini-files.lua
@@ -0,0 +1,17 @@
+return {
+  "echasnovski/mini.files",
+  keys = {
+    {
+      "<leader>e",
+      function()
+        require('mini.files').open(vim.api.nvim_buf_get_name(0), true)
+      end,
+      desc = "Open mini.files (directory of current file)",
+    },
+  },
+  opts = {
+    mappings = {
+      go_in =
+    }
+  }
+}
