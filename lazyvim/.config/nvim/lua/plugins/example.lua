f6ccd8ff40079dfa8cb27bb5d87f6d0e5a6e4820
aviralmansingka <aviralmansingka@gmail.com>
2025-08-13 14:47:01 -0400

    Removing starter ui

--- lazyvim/.config/nvim/lua/plugins/example.lua
+++ lazyvim/.config/nvim/lua/plugins/example.lua
@@ -106,9 +106,6 @@ return {
     end,
   },
 
-  -- use mini.starter instead of alpha
-  { import = "lazyvim.plugins.extras.ui.mini-starter" },
-
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
   { import = "lazyvim.plugins.extras.lang.json" },
 
