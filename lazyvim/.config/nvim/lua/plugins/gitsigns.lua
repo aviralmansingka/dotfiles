63a6d0e750827cbabae44abfed64efd90aa376e3
Aviral Mansingka <aviral@modal.com>
2025-08-15 20:20:22 +0000

    Updating gitsigns

--- lazyvim/.config/nvim/lua/plugins/gitsigns.lua
+++ lazyvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -8,11 +8,11 @@ return {
     "lewis6991/gitsigns.nvim",
     opts = {
       signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
+        add = { text = "┃" },
+        change = { text = "┃" },
+        delete = { text = "━" },
+        topdelete = { text = "━" },
+        changedelete = { text = "┃" },
       },
       on_attach = function(bufnr)
         local gitsigns = require("gitsigns")
