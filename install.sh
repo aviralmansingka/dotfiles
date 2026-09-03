9f370b12998344d97331c07766e37ee9b6c82a7b
Aviral Mansingka <aviralmansingka@gmail.com>
2026-09-03 16:06:17 -0400

    chore: remove karabiner-elements stow package (Nuphy BT ungrabbable, mods not worth keeping)

--- install.sh
+++ install.sh
@@ -99,7 +99,6 @@ stow -d "$DOTFILES_DIR" -t "$HOME" nvim
 stow -d "$DOTFILES_DIR" -t "$HOME" starship
 stow -d "$DOTFILES_DIR" -t "$HOME" ghostty
 stow -d "$DOTFILES_DIR" -t "$HOME" aerospace
-stow -d "$DOTFILES_DIR" -t "$HOME" karabiner
 stow -d "$DOTFILES_DIR" -t "$HOME" gh-dash
 stow -d "$DOTFILES_DIR" -t "$HOME" agents
 stow -d "$DOTFILES_DIR" -t "$HOME" pi
